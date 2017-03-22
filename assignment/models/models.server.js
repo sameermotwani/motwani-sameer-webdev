3e6277aefb4516c1a7c9bd6c13f18c40448d0cdf
Sameer Motwani <sameermotwani11@gmail.com>
2017-03-22 15:57:20 -0400

    Added deployment changes V 5.3
    Added deployment changes V 5.3

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,12 +1,14 @@
 module.exports = function () {
 
     var connectionString = 'mongodb://127.0.0.1:27017/webAppMaker';
-    // if(process.env.MLAB_USERNAME) {
-    //     connectionString = process.env.MLAB_USERNAME + ":" +
-    //         process.env.MLAB_PASSWORD + "@" +
-    //         process.env.MLAB_HOST + ':' +
-    //         process.env.MLAB_PORT + '/' +
-    //         process.env.MLAB_APP_NAME;
+     if(process.env.MLAB_USERNAME) {
+         connectionString = process.env.MLAB_USERNAME + ":" +
+             process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+             process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+	console.log(connectionString);
+	mongoose.connect(connectionString);
     // }
     console.log(connectionString);
 
@@ -15,7 +17,7 @@ module.exports = function () {
     //mongoose.Promise = global.Promise;
 
    // var options = { promiseLibrary: require('bluebird') };
-    mongoose.connect(connectionString);
+   // mongoose.connect(connectionString);
     //var Promise = require('bluebird');
     //mongoose.Promise = global.Promise;
    // var options = { promiseLibrary: require('bluebird') };
